69324a5b37b304194a71b115775b29e90fda63f6
Danniel Martinez <111100025+d4lion@users.noreply.github.com>
2026-06-21 23:37:02 -0500

    Update startup command in web.py

--- web.py
+++ web.py
@@ -3,7 +3,7 @@ from main import lambda_handler
 
 app = FastAPI()
 
-# Start with: uvicorn test_web:app --host 0.0.0.0 --port 8000
+# Start with: uvicorn web:app --host 0.0.0.0 --port 8000
 @app.post("/wompi/webhook")
 async def wompi_webhook(request: Request):
     payload = await request.json()
